97b86d73836aeda3d94b6572cf85dc8aea7e7e72
peppapig450 <nbgenius1@gmail.com>
2024-08-10 13:26:45 -0700

    add pinecone document uploader that is ran when the document store is empty and use pinecone for the vectorStore retriever

--- src/lib/loadDocuments.ts
+++ src/lib/loadDocuments.ts
@@ -1,8 +1,9 @@
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { JSONLoader } from "langchain/document_loaders/fs/json";
+import { PineconeStore } from "@langchain/pinecone";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { getPineconeIndex } from "@/lib/pinecone";
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import path from "path";
 
 const embeddings = new HuggingFaceInferenceEmbeddings({
@@ -10,7 +11,9 @@ const embeddings = new HuggingFaceInferenceEmbeddings({
   model: "sentence-transformers/multi-qa-mpnet-base-dot-v1",
 });
 
-export async function getRetriever() {
+export async function uploadToPinecone() {
+  const pineconeIndex = getPineconeIndex(process.env.PINECONE_INDEX!);
+
   const dataDirectory = path.join(process.cwd(), "data/");
 
   const loader = new DirectoryLoader(dataDirectory, {
@@ -19,6 +22,7 @@ export async function getRetriever() {
 
   const docs = await loader.load();
 
+  // TODO: custom json splitting for better metadata ?
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
     chunkOverlap: 200,
@@ -26,15 +30,33 @@ export async function getRetriever() {
 
   const allSplits = await textSplitter.splitDocuments(docs);
 
-  const vectorStore = await MemoryVectorStore.fromDocuments(
-    allSplits,
-    embeddings
-  );
+  await PineconeStore.fromDocuments(allSplits, embeddings, {
+    pineconeIndex: pineconeIndex,
+    textKey: "text",
+  });
+}
 
-  const retriever = vectorStore.asRetriever({
-    k: 8,
-    searchType: "similarity",
+export async function getRetriever() {
+  const pineconeIndex = getPineconeIndex(process.env.PINECONE_INDEX!);
+
+  const indexStats = await pineconeIndex.describeIndexStats();
+  const vectorCount = indexStats.totalRecordCount;
+
+  // TODO: find better way to ensure all documents are uploaded
+  if (vectorCount === 0) {
+    console.log("No documents found in Pinecone. Uploading documents...");
+    await uploadToPinecone();
+  } else {
+    console.log("Documents already exist in Pinecone. No upload needed.");
+  }
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex: pineconeIndex,
+    textKey: "text",
   });
 
-  return retriever;
+  return vectorStore.asRetriever({
+    k: 10,
+    searchType: "similarity",
+  });
 }
