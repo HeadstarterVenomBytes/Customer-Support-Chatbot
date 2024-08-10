2eaa02d7811c20d76a1b968d8bdbe4c72198e42a
peppapig450 <nbgenius1@gmail.com>
2024-08-10 15:28:41 -0700

    get rid of pinecone client as we're not using pinecone anymore

--- src/lib/pinecone.ts
+++
@@ -1,9 +0,0 @@
-import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
-
-const pinecone = new PineconeClient({
-  apiKey: process.env.PINECONE_API_KEY!,
-});
-
-export const getPineconeIndex = (indexName: string) => {
-  return pinecone.Index(indexName);
-};
